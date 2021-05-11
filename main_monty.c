9a4236776733ecd40f56bca799a9299b5cff1bf7
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-11 04:26:33 +0300

    Create main_monty.c

---
+++ main_monty.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "monty.h"
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	return (0);
+}
