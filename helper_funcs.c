f888874b46638a3094a28eea281d524ecc8415f3
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-11 16:35:04 +0300

    added add_node_end

--- helper_funcs.c
+++ helper_funcs.c
@@ -27,6 +27,37 @@ int is_valid(char *line)
 }
 
 /**
+ * add_node_end - adds a node to the end of a linked list
+ * @head: pointer to the head of the list
+ * @str: string to be used as content of the newly added node
+ *
+ * Return: address of the newly added node
+ */
+list_t *add_node_end(list_t **head, char *str)
+{
+	list_t *new_node;
+	list_t *cursor = *head;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node != NULL)
+	{
+		new_node->str = strdup(str);
+		new_node->next = NULL;
+	}
+	else
+		return (NULL);
+	if (cursor != NULL)
+	{
+		while (cursor->next != NULL)
+			cursor = cursor->next;
+		cursor->next = new_node;
+	}
+	else
+		*head = new_node;
+	return (new_node);
+}
+
+/**
  * remove_newline - removes new line from a string
  * @str: string to be used
  *
