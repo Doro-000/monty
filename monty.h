b983e18e0f151a5a213f5f885273ce8fce2f65d8
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-11 16:26:14 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -25,9 +25,15 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/* stack_queue */
 stack_t *push_value(stack_t **head, int n);
 stack_t *pop_value(stack_t **head);
 void print_stack(stack_t *h);
 stack_t *get_value(stack_t *head);
+
+/* helper_funcs */
+int is_valid(char *line);
+void remove_newline(char *str);
+
 
 #endif /*MONTY_H*/
