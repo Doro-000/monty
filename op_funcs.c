8e319b8848a54cd7f73ff07094d4aff68a92151c
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-12 14:38:04 +0300

    update push function

--- op_funcs.c
+++ op_funcs.c
@@ -1,12 +1,21 @@
 #include "monty.h"
+#include <stdio.h>
 
 
+/**
+ * push - push a value to the main stack
+ * @stack: top of the stack/ head of doubly linked list
+ * @line_number: line number of the instruction
+ *
+ * Return: void
+ */
 void push(stack_t **stack, unsigned int line_number)
 {
 	int operand = 0;
 	int *operand_ptr = &operand;
 	list_t *instruction = NULL;
 	list_t *cursor = monty_file_content;
+	stack_t *top = NULL;
 
 	while (cursor != NULL)
 	{
@@ -16,6 +25,20 @@ void push(stack_t **stack, unsigned int line_number)
 			break;
 		}
 	}
-	get_operand(instruction->str, operand_ptr)
-	
+	get_operand(instruction->str, operand_ptr);
+	if (operand_ptr == NULL)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		free(monty_file_content);
+		/*free stack*/
+		exit(EXIT_FAILURE);
+	}
+	top = push_value(stack, operand);
+	if (top == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free(monty_file_content);
+		/*free stack*/
+		exit(EXIT_FAILURE);
+	}
 }
