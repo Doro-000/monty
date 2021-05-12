c8d56f9f5fde3e689e8fd23b6ef774a7cc4a8a2e
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-12 05:22:01 +0300

    Create instruction_manip.c

---
+++ instruction_manip.c
@@ -0,0 +1,49 @@
+#include "monty.h"
+
+
+/**
+ * get_opcode - gets the opcode from an instruction
+ * @instruction: instruction to be checked
+ * @store: buffer to store the opcode in
+ *
+ * Return: void
+ */
+void get_opcode(char *instruction, char *store)
+{
+	char *temp = strdup(instruction);
+	strcpy(store, strtok(temp, " "));
+	free(temp);
+}
+
+
+/**
+ * get_operand - gets the operand from an instruction
+ * @instruction: instruction to be checked
+ * @store: integer to store the operand in
+ *
+ * Return: void
+ */
+void get_operand(char *instruction, int *store)
+{
+	char *temp = strdup(instruction);
+	char *temp_2 = strtok(temp, " ");
+	int flag = 0;
+	temp_2 = strtok(NULL, " ");
+	if (temp_2 == NULL)
+		store = NULL
+	else
+	{
+		while (*temp_2)
+		{
+			if ((*temp_2 < 48) && (*temp_2 > 57))
+			{
+				flag = 1;
+				store = NULL;
+				break;
+			}
+		}
+	}
+	if (!flag)
+		*store = atoi(temp_2);
+	free(temp);
+}
