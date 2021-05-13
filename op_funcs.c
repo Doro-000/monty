9deb1335d138534e06a26f1f40c92ab46b294267
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-13 15:25:54 +0300

    added new functions for more opcodes

--- op_funcs.c
+++ op_funcs.c
@@ -62,3 +62,68 @@ void pall(stack_t **stack, unsigned int line_number __attribute__((unused)))
 		cursor = cursor->next;
 	}
 }
+
+/**
+ * pint - prints the value at the top of the stack
+ * @stack: top of the stack/ head of doubly linked list
+ * @line_number: line number of the instruction
+ *
+ * Return: void
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		free_stack(*stack); /*probably unecessary*/
+		free_list(monty_file_content);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
+
+/**
+ * pop - removes the top element of the stack
+ * @stack: top of the stack/ head of doubly linked list
+ * @line_number: line number of the instruction
+ *
+ * Return: void
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *popped = NULL;
+
+	popped = pop_value(stack);
+	if (popped == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		free_stack(*stack);
+		free_list(monty_file_content);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * swap - swaps the top two elements of the stack
+ * @stack: top of the stack/ head of doubly linked list
+ * @line_number: line number of the instruction
+ *
+ * Return: void
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	if (stack_len(*stack) < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		free_stack(*stack);
+		free_list(monty_file_content);
+		exit(EXIT_FAILURE);
+	}
+	stack_t *temp_1 = NULL, *temp_2 = NULL;
+	temp_1 = pop_value(stack);
+	temp_2 = pop_value(stack);
+	push_value(stack, temp_1->n);
+	push_value(stack, temp_2->n);
+}
+
+
