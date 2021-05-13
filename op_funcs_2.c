2ac6a3e66f2ec3b7dacd648fc161adad8814fdff
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-13 20:28:37 +0300

    added functions mul, div, sub

--- op_funcs_2.c
+++ op_funcs_2.c
@@ -2,6 +2,19 @@
 
 
 /**
+ * nop - doesn't do anything
+ * @stack: top of the stack/ head of doubly linked list
+ * @line_number: line number of the instruction
+ *
+ * Return: void
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	stack = stack;
+	line_number = line_number;
+}
+
+/**
  * add - adds the top two elements of the stack
  * @stack: top of the stack/ head of doubly linked list
  * @line_number: line number of the instruction
@@ -27,14 +40,84 @@ void add(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * nop - doesn't do anything
+ * sub - subtracts the top two elements of the stack
  * @stack: top of the stack/ head of doubly linked list
  * @line_number: line number of the instruction
  *
  * Return: void
  */
-void nop(stack_t **stack, unsigned int line_number)
+void sub(stack_t **stack, unsigned int line_number)
 {
-	stack = stack;
-	line_number = line_number;
+	stack_t *temp_1 = NULL, *temp_2 = NULL;
+
+	if (stack_len(*stack) < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		free_stack(*stack);
+		free_list(monty_file_content);
+		exit(EXIT_FAILURE);
+	}
+	temp_1 = pop_value(stack);
+	temp_2 = pop_value(stack);
+	push_value(stack, temp_2->n - temp_1->n);
+	free(temp_1);
+	free(temp_2);
+}
+
+/**
+ * mul - multiplies the top two elements of the stack
+ * @stack: top of the stack/ head of doubly linked list
+ * @line_number: line number of the instruction
+ *
+ * Return: void
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp_1 = NULL, *temp_2 = NULL;
+
+	if (stack_len(*stack) < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		free_stack(*stack);
+		free_list(monty_file_content);
+		exit(EXIT_FAILURE);
+	}
+	temp_1 = pop_value(stack);
+	temp_2 = pop_value(stack);
+	push_value(stack, temp_1->n * temp_2->n);
+	free(temp_1);
+	free(temp_2);
+}
+
+/**
+ * div - divides the top two elements of the stack
+ * @stack: top of the stack/ head of doubly linked list
+ * @line_number: line number of the instruction
+ *
+ * Return: void
+ */
+void div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp_1 = NULL, *temp_2 = NULL;
+
+	if (stack_len(*stack) < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		free_stack(*stack);
+		free_list(monty_file_content);
+		exit(EXIT_FAILURE);
+	}
+	temp_1 = pop_value(stack);
+	if (temp_1->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero", line_number);
+		free(temp_1);
+		free_stack(*stack);
+		free_list(monty_file_content);
+		exit(EXIT_FAILURE);
+	}
+	temp_2 = pop_value(stack);
+	push_value(stack, temp_2->n / temp_1->n);
+	free(temp_1);
+	free(temp_2);
 }
