6ad8cbcef329b7a1b95351cb913782f17a5e7286
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-05-13 21:06:12 +0300

    file for more helper funcs

---
+++ helper_funcs_2.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+
+/**
+ * is_comment - checks if a string is a commend
+ * @line: string to be checked
+ *
+ * Return: 1 if comment, 0 otherwise
+ */
+int is_comment(char *line)
+{
+	char *temp = strdup(line);
+	char *temp_2 = strok(temp, " ");
+
+	if (temp_2 == NULL)
+	{
+		free(temp);
+		return (0);
+	}
+	if (*temp_2 == '#')
+		return (1);
+	return (0);
+}
